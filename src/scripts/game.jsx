d6b0f55b3530e966220725edc90f5850f534ea72
Pasha Sadikov <pashalab@gmail.com>
2019-12-29 12:52:54 -0500

    Replace pixi-react with own component

---
+++ src/scripts/game.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import * as PIXI from 'pixi.js';
+
+export default class Game extends React.Component {
+    componentDidMount() {
+        // Create a Pixi Application
+        let type = "WebGL";
+        if(!PIXI.utils.isWebGLSupported()){
+            type = "canvas";
+        }
+        PIXI.utils.sayHello(type);
+
+        let app = new PIXI.Application({
+            width: this.container.offsetWidth,
+            height: this.container.offsetHeight,
+        });
+        const element = document.getElementById('game-container');
+        element.parentNode.replaceChild(app.view, element);
+        this.setState = { app };
+    }
+
+    render() {
+        return (
+            <div id="game-container"
+                 className="pixiStage"
+                 ref={el => (this.container = el)}/>
+        );
+    }
+}
--- src/scripts/main.jsx
+++ src/scripts/main.jsx
@@ -1,36 +1,18 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { Stage, Text, Sprite } from '@inlet/react-pixi';
 
 import Header from './header.jsx';
 import Footer from './footer.jsx';
+import Game from './game.jsx';
 
-const BasicText = () => (
-  <Text x={30} y={90} text="Basic text in pixi" />
-);
-
-class Content extends Component {
-    render() {
-        return (
-            <Stage options={{ backgroundColor: 0xffffff }}>
-              <BasicText />
-              <Sprite image="assets/images/oval.svg"/>
-            </Stage>
-        );
-    }
-}
 
 class App extends Component {
     render() {
-        var page = {
-            header: "Pixi Template",
-            contents: "Game goes here"
-        };
         return (
             <div className="main-wrapper">
-              <Header header={page.header}/>
-              <Content contents={page.contents}/>
+              <Header header="pixi.js Game"/>
+              <Game/>
               <Footer/>
             </div>
         );
